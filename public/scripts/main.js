1e7c0db0bdee8ebb8edfbc9f17de500c3ef6b857
mpetty <michael.petty@gmail.com>
2020-04-05 23:10:49 -0700

    add random question route; fe consume and display random question

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('Hello from the client-side');
+
+const asyncFetch = async (url, method='get') => {
+  const res = await fetch(url, {method: method});
+  return await res.json();
+}
+
+const renderQuestion = (data) => {
+  const section = document.querySelector('.questions');
+  section.insertAdjacentHTML('afterbegin', `<article class="question">${data.content}</article>`)
+
+}
+
+const getQuestion = () => {
+  asyncFetch('/api/v1/questions/random')
+    .then(res => {
+      renderQuestion(res.data);
+    })
+    .catch(err =>  console.log(err));
+}
+
+getQuestion();
--- server.js
+++ server.js
@@ -12,4 +12,18 @@ app.get('/', (req,res) => {
   })
 })
 
+// temp data
+const tempQuestion = {id: 1, account_id: 1, content: 'What is your favorite color? And how is that choice reflected in your life?', name: 'Jimmy'};
+
+app.get('/api/v1/questions', (req, res) => res.json({status: 200, message:'i got your questions', data: {}}));
+
+app.get('/api/v1/questions/:id', (req, res) => {
+  if (req.params.id === 'random') {
+    res.json({status: 200, message: 'one random question', data: tempQuestion})
+  } else {
+    res.json({status: 200, message: `data for question id: ${req.params.id}`, data: {}})
+  }
+
+})
+
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
